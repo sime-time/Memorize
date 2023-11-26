ab731c4ed9d8d097690573e184fb22dfe2b95e54
Dunn, Simeon <simedunn01@gmail.com>
2023-11-26 09:37:01 -0500

    CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack {
             CardView()
             CardView(isFaceUp: true)
+            CardView()
         }
     }
 }
